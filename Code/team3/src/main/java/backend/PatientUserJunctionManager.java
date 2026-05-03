93f4ec4a65a1fc49780e94ff32f28e35606d37d0
MichaelHuang478 <huang110@csusm.edu>
2026-05-02 23:08:58 -0700

    change

--- Code/team3/src/main/java/backend/PatientUserJunctionManager.java
+++ Code/team3/src/main/java/backend/PatientUserJunctionManager.java
@@ -23,7 +23,51 @@ public class PatientUserJunctionManager {
         return patientUserJunctions.get(id);
     }
 
+    public Map<Integer, PatientUserJunction> getAllAssignments() {
+        return patientUserJunctions;
+    }
+
+    public PatientUserJunction findByPatientID(int patientID) {
+        for (PatientUserJunction a : patientUserJunctions.values()) {
+            if (a.getPatientID() == patientID) {
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public PatientUserJunction findByUserID(int userID) {
+        for (PatientUserJunction a : patientUserJunctions.values()) {
+            if (a.getUserID() == userID) {
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public void updateAssignment(int id, PatientUserJunction updatedAssignment) {
+        if (!patientUserJunctions.containsKey(id)) {
+            throw new RuntimeException("Assignment not found");
+        }
+
+        patientUserJunctions.put(id, updatedAssignment);
+    }
+
+    public void updateAssignmentTime(int id, String time) {
+        PatientUserJunction a = getAssignment(id);
+        a.setAssignmentTime(time);
+    }
+
+    public void updateAssignmentEnd(int id, String end) {
+        PatientUserJunction a = getAssignment(id);
+        a.setAssignmentEnd(end);
+    }
+
     public void removeAssignment(int id) {
+        if (!patientUserJunctions.containsKey(id)) {
+            throw new RuntimeException("Assignment not found");
+        }
+
         patientUserJunctions.remove(id);
     }
 }
